27f29b810eb012ae84565374ab8c506048e75f93
Bhargav Dasari <bhargavsaidasari@gmail.com>
2025-05-24 19:01:59 -0600

    Add logging for skipped files

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,11 @@ mod embed;
 use clap::Parser;
 use cli::{Args};
 use search::search_files;
+use env_logger;
+
 
 fn main() {
+    env_logger::init();
     let args = Args::parse();
     search_files(&args);
 }
--- src/search.rs
+++ src/search.rs
@@ -2,6 +2,7 @@ use crate::cli::{SearchMode, Args};
 use crate::embed::{cosine_similarity, embed_text};
 use walkdir::WalkDir;
 use std::fs;
+use log::{info, debug, warn, error};
 
 
 pub fn search_files(args: &Args){
@@ -15,14 +16,21 @@ pub fn search_files(args: &Args){
             let path = entry.path();
             let result_text = match args.mode{
                 SearchMode::Name => path.file_name().unwrap_or_default().to_string_lossy().to_string(),
-                SearchMode::Content => fs::read_to_string(path).unwrap_or_default()
+                SearchMode::Content => 
+                    match fs::read_to_string(path) {
+                        Ok(content) => content,
+                        Err(err) => {
+                            debug!(" Skipping {}: {}", path.display(), err);
+                            String::from("")
+                        }
+                }
             };
         
         let file_vec = embed_text(&result_text);
         let score = cosine_similarity(&query_vec, &file_vec);
         
         if score > 0.7{
-            println!("{:.2} -> {}", score, path.display());
+            info!("{:.2} -> {}", score, path.display());
         }
         //"Make Score configurable from CLI to let user define what value they want"
 
